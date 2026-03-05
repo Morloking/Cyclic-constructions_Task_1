9c325c8c64e665c92b9165fc0513e02bb826c7c8
Morloking <dyubenkosergei@yandex.ru>
2026-03-05 23:43:48 +0300

    Add files via upload

---
+++ sector.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    setlocale(LC_ALL, "RUSSIAN");
+    int num{10}; 
+    int summ{0};
+    do {
+        std::cout << "¬ведите целое число или число '0', чтобы закончить:\n";
+        std::cin >> num;
+        summ += num;
+        std::cin.clear();
+        std::cin.ignore(32767, '\n');
+    } while (num != 0);
+    std::cout << "—умма: " << summ << "\n";
+}
